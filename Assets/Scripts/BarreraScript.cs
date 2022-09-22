93789a68762a6b0cbfe257ae58ae479b091c1983
LeoBoro2595 <leoboro1234@gmail.com>
2022-09-22 09:02:47 -0300

    Initial commit

---
+++ Assets/Scripts/BarreraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarreraScript : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter()
+    {
+        Debug.Log("Ingreso detectado");
+        anim.SetBool("estaAdentro", true);
+    }
+
+    private void OnTriggerExit()
+    {
+        Debug.Log("Salida detectada");
+        anim.SetBool("estaAdentro", false);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Clase Animacion-2
+ 
